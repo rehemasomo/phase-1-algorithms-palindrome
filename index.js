63c284c8b6304b67c811a09c3d8248e5e76ee7e9
rehemasomo <rehemasomo30@gmail.com>
2024-04-07 08:31:15 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
   // Write your algorithm here
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+  
+  // Reverse the string
+  const reversedStr = str.split('').reverse().join('');
+  
+  // Check if the original string is equal to the reversed string
+  return str === reversedStr;
 }
 
 /* 
@@ -8,18 +16,12 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
-*/
+*/ 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
+  console.log(isPalindrome("madam")); 
+  console.log(isPalindrome("robot"));
+ }
 module.exports = isPalindrome;
